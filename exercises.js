a0cd7ef8c77a92f592ee45b0ff09deb23f5dd11b
Brett Macy <bam03584@aol.com>
2016-07-27 13:43:15 -0400

    added index.html for live load console response

---
+++ exercises.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/**
+ * For each of the following Array methods,
+
+ * 1. use them in an example
+ * 2. use console.log() to make the code run in the console!
+
+ **/
+const array = [1,2,3]
+
+ console.log(array);
+// .sort()
+
+
+// .concat()
+
+// .indexOf()
+
+// .split()
+
+// .join()
+
+// .pop()
+
+// .push()
+
+// .slice()
+
+// .splice()
+
+// .shift()
+
+// .unshift()
+
+// .filter()
+
+// .map()
